002fd29de24a28984422aabcc61a750ca9985b3b
=t2a5vvw <=winkler_it@gmx.de>
2019-10-30 13:44:35 +0100

    New PHP Update

---
+++ php/class/todoClass.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+class todoClass{
+
+  public function __construct() {
+    echo "TODO Class erzeugt";
+  }
+
+}
+
+
+?>
--- php/start.php
+++ php/start.php
@@ -1,6 +1,6 @@
 <?php
-
+include("function.php");
 echo "Hello World";
 
 
- ?>
+?>
